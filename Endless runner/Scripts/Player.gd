fcb3a8d28c1d1f44a66b2ed9c911fa3305032b1b
vickraz <victor.oberg@minervagymnasium.se>
2024-09-04 09:16:48 +0200

    Lektion 3: coyote_timer och run animation

--- Endless runner/Scripts/Player.gd
+++ Endless runner/Scripts/Player.gd
@@ -15,6 +15,9 @@ var velocity: Vector2 = Vector2.ZERO
 var gravity: int = NORM_GRAVITY
 var distance: float = 0.0
 var state: int = RUN
+var can_jump: bool = true
+
+onready var coyote_timer: Timer = $CoyoteTimer
 
 
 func _physics_process(delta: float) -> void:
@@ -38,10 +41,30 @@ func _move_player(delta: float) -> void:
 ################## STATE FUNCTIONS ####################################
 
 func _run_state(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Jump"):
+		_enter_air_state(true)
+		_move_player(delta)
+		return
+	
+	_move_player(delta)
+	
+	if not is_on_floor():
+		_enter_air_state(false)
 
 func _air_state(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Jump") and can_jump:
+		_enter_air_state(true)
+	elif Input.is_action_just_released("Jump"):
+		gravity = NORM_GRAVITY
+	if Input.is_action_pressed("Down"):
+		gravity = MAX_GRAVITY
+	elif Input.is_action_just_released("Down"):
+		gravity = NORM_GRAVITY
+	
+	_move_player(delta)
+	
+	if is_on_floor():
+		_enter_run_state()
 
 func _dead_state() -> void:
 	pass
@@ -50,25 +73,24 @@ func _dead_state() -> void:
 ################## ENTER STATE FUNCTIONS ##############################
 func _enter_run_state() -> void:
 	state = RUN
+	gravity = NORM_GRAVITY
 
 func _enter_air_state(jumping: bool) -> void:
 	state = AIR
 	if jumping:
+		can_jump = false
+		gravity = MIN_GRAVITY
 		velocity.y = -JUMP_STRENGTH
+	else:
+		gravity = NORM_GRAVITY
+		can_jump = true
+		coyote_timer.start()
 
 func enter_dead_state() -> void:
 	state = DEAD
 
 
-	"""
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = -JUMP_STRENGTH
-		gravity = MIN_GRAVITY
-	elif Input.is_action_just_released("Jump"):
-		gravity = NORM_GRAVITY
-	
-	if Input.is_action_pressed("Down") and not is_on_floor():
-		gravity = MAX_GRAVITY
-	
-	_move_player(delta)
-"""
+
+####################### SIGNALS ############################################
+func _on_CoyoteTimer_timeout() -> void:
+	can_jump = false
